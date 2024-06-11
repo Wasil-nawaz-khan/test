bf6c5238c6d8c290fd783b291f809a4c7693e417
wasil nawaz khan <wasilnawazk@gmail.com>
2024-06-11 14:46:58 +0530

    Add files via upload

---
+++ myproject1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1>Hello world</h1>
+    <p>Welocme to my project 1</p>
+    <p>kasfkaifh kafkiesf kafg</p>
+    <footer>Copyright no one &copy;</footer>
+    </div>
+  
+  );
+}
+
+export default App;
